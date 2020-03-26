3e82430aab578b9ea4085fb7820e604baaf75365
Shiv Patel <shivgp11@iastate.edu>
2020-03-26 11:48:14 -0500

    updated p2

--- COM_S_327/project_p2/Source Files/checkMoves.c
+++ COM_S_327/project_p2/Source Files/checkMoves.c
@@ -18,6 +18,7 @@ extern int line;
 extern int readMoves;
 extern int turnNum;
 extern int limit;
+extern int checkMovesIs2;
 
 int checkMoves(FILE *fp)
 {
@@ -49,16 +50,10 @@ int checkMoves(FILE *fp)
 		if (r == '.')
 		{
 			numMoves++;
-			if(turnNum == 1){
-				if(validateDot() == 0) {
-					fprintf(stderr, "Move %d is illegal: %c\n", numMoves, r);
-					return 0;
-				}
-			} else{
-				if(validateDot() == 0) {
-					fprintf(stderr, "Move %d is illegal: %c\n", numMoves, r);
-					return 0;
-				}
+			if (validateDot() == 0)
+			{
+				fprintf(stderr, "Move %d is illegal: %c\n", numMoves, r);
+				return 0;
 			}
 			parse(fp);
 			continue;
@@ -68,11 +63,15 @@ int checkMoves(FILE *fp)
 		{
 			currentResets++;
 			numMoves++;
-			if (currentResets > limit){
+			if (currentResets > limit)
+			{
 				fprintf(stderr, "Move %d is illegal: %c\n", numMoves, r);
 				return 0;
-			} else {
-				if(reset() == 0) {
+			}
+			else
+			{
+				if (reset() == 0)
+				{
 					fprintf(stderr, "Move %d is illegal: %c\n", numMoves, r);
 					return 0;
 				}
@@ -123,23 +122,28 @@ int checkMoves(FILE *fp)
 		}
 
 		numMoves++;
-		if (validateMove(currentMove) == 0){
+		if (validateMove(currentMove) == 0)
+		{
 			fprintf(stderr, "Move %d is illegal: %s\n", numMoves, currentMove);
-			return 0;
+			checkMovesIs2 = 1;
+			return 1;
 		}
 		parse(fp);
 	}
 	return 1;
 }
 
-int reset() {
-	if(stockStack != NULL) {
+int reset()
+{
+	if (stockStack != NULL)
+	{
 		return 0;
 	}
-	node* current = wasteStack;
-	node* previous = NULL;
-	node* next = NULL;
-	while(current != NULL) {
+	node *current = wasteStack;
+	node *previous = NULL;
+	node *next = NULL;
+	while (current != NULL)
+	{
 		next = current->next;
 		current->next = previous;
 		previous = current;
@@ -147,247 +151,349 @@ int reset() {
 	}
 	wasteStack = previous;
 	stockStack = wasteStack;
+	fprintf(stdout, "stack is %c%c\n", stockStack->rank, stockStack->suit);
 	wasteStack = NULL;
-	
+
 	return 1;
 }
 
-int validateDot() {
-	if(stockStack == NULL) {
+int validateDot()
+{
+	if (stockStack == NULL)
+	{
 		return 0;
 	}
-	node* current = NULL;
-	node* next = NULL;
-	if(wasteStack == NULL) {
-		current = stockStack;
-		next = current->next;
-		current->next = NULL;
-		wasteStack = current;
-		stockStack = next;
+	node *current = NULL;
+	node *next = NULL;
+	for (int i = 0; i < turnNum; i++)
+	{
+		if (wasteStack == NULL)
+		{
+			current = stockStack;
+			next = current->next;
+			current->next = NULL;
+			wasteStack = current;
+			stockStack = next;
+		}
+		else
+		{
+			current = stockStack;
+			next = current->next;
+			current->next = wasteStack;
+			wasteStack = current;
+			stockStack = next;
+		}
+		if (stockStack == NULL)
+		{
+			break;
+		}
 	}
-	else {
-		current = stockStack;
-		next = current->next;
-		current->next = wasteStack;
-		wasteStack = current;
-		stockStack = next;
-	}
+	//fprintf(stdout, "stack is %c%c\n", stockStack->rank,  stockStack->suit);
 	return 1;
 }
 
-int validateMove(char* str){
+int validateMove(char *str)
+{
 	int src = str[0] - '0';
 	int dest = str[3] - '0';
-	node* next = NULL;
-	node* current = NULL;
-	node* prev = NULL;
-	node* end = NULL;
-	if(str[0] == 'w' && str[3] == 'f'){
-		if(wasteStack == NULL) {
+	node *next = NULL;
+	node *current = NULL;
+	node *prev = NULL;
+	node *end = NULL;
+	if (str[0] == 'w' && str[3] == 'f')
+	{
+		if (wasteStack == NULL)
+		{
 			return 0;
 		}
-			
-		if(wasteStack->rank == 'A') {
-			if(wasteStack->suit == 'c') {
+		if (wasteStack->rank == 'A')
+		{
+			if (wasteStack->suit == 'c')
+			{
+				// printf("clubs\n");
+				// printf("start\n");
 				current = wasteStack;
 				next = current->next;
 				current->next = NULL;
 				clubsFoundation = current;
-				wasteStack = next;	
-				return 1;
+				wasteStack = next;
+				//printf("finish\n");
 			}
-			else if(wasteStack->suit == 'd') {
+			else if (wasteStack->suit == 'd')
+			{
+				// printf("diamonds\n");
+				// printf("start\n");
 				current = wasteStack;
 				next = current->next;
 				current->next = NULL;
 				diamondsFoundation = current;
 				wasteStack = next;
-				return 1;
+				//printf("finish\n");
 			}
-			else if(wasteStack->suit == 'h') {
+			else if (wasteStack->suit == 'h')
+			{
+				// printf("hearts\n");
+				// printf("start\n");
 				current = wasteStack;
 				next = current->next;
 				current->next = NULL;
 				heartsFoundation = current;
 				wasteStack = next;
-				return 1;
+				//printf("finish\n");
 			}
-			else {
+			else if (wasteStack->suit == 's')
+			{
+				// printf("spades\n");
+				// printf("start\n");
 				current = wasteStack;
 				next = current->next;
 				current->next = NULL;
 				spadesFoundation = current;
 				wasteStack = next;
-				return 1;
+				//printf("finish\n");
 			}
 		}
-		if(compareFoundationCards(wasteStack, clubsFoundation) == 1) {
-			
+		else if (compareFoundationCards(wasteStack, clubsFoundation) == 1)
+		{
+			// printf("clubs\n");
+			// printf("start\n");
 			current = wasteStack;
 			next = current->next;
 			current->next = clubsFoundation;
 			clubsFoundation = current;
 			wasteStack = next;
+			//printf("finish\n");
 		}
-		else if(compareFoundationCards(wasteStack, diamondsFoundation) == 1) {
+		else if (compareFoundationCards(wasteStack, diamondsFoundation) == 1)
+		{
+			// printf("diamonds\n");
+			// printf("start\n");
 			current = wasteStack;
 			next = current->next;
 			current->next = diamondsFoundation;
 			diamondsFoundation = current;
 			wasteStack = next;
+			//printf("finish\n");
 		}
-		else if(compareFoundationCards(wasteStack, heartsFoundation) == 1) {
+		else if (compareFoundationCards(wasteStack, heartsFoundation) == 1)
+		{
+			// printf("heart\n");
+			// printf("start\n");
 			current = wasteStack;
 			next = current->next;
 			current->next = heartsFoundation;
 			heartsFoundation = current;
 			wasteStack = next;
+			//printf("finish\n");
 		}
-		else if(compareFoundationCards(wasteStack, spadesFoundation) == 1) {
+		else if (compareFoundationCards(wasteStack, spadesFoundation) == 1)
+		{
+			// printf("spades\n");
+			// printf("start\n");
 			current = wasteStack;
 			next = current->next;
 			current->next = spadesFoundation;
 			spadesFoundation = current;
 			wasteStack = next;
+			//printf("finish\n");
 		}
-		else {
+		else
+		{
 			return 0;
 		}
-	} else if (str[0] == 'w' && (dest > 0 && dest < 8)){
-		if(wasteStack == 0) {
+	}
+	else if (str[0] == 'w' && (dest > 0 && dest < 8))
+	{
+		if (wasteStack == 0)
+		{
 			return 0;
 		}
-		if((wasteStack->rank) == 'K' && uncoveredTable[7 - dest] == NULL) {
+		if ((wasteStack->rank) == 'K' && uncoveredTable[7 - dest] == NULL)
+		{
 			current = wasteStack;
 			next = current->next;
 			current->next = NULL;
 			uncoveredTable[7 - dest] = current;
 			wasteStack = next;
 			return 1;
-			
 		}
+		else if ((wasteStack->rank) != 'K' && uncoveredTable[7 - dest] == NULL)
+		{
+			return 0;
+		}
+
 		end = uncoveredTable[7 - dest];
-		while(end->next != NULL) {
+		while (end->next != NULL)
+		{
 			end = end->next;
 		}
-		if(compareCards(wasteStack, end) == 1){
+		if (compareCards(wasteStack, end) == 1)
+		{
 			current = wasteStack;
 			next = current->next;
 			end->next = current;
 			current->next = NULL;
 			wasteStack = next;
-			
-		} else {
+		}
+		else
+		{
 			return 0;
 		}
-	} else if ((src > 0 && src < 8) && str[3] == 'f') {
-		if(uncoveredTable[7 - src] == NULL) {
+	}
+	else if ((src > 0 && src < 8) && str[3] == 'f')
+	{
+		if (uncoveredTable[7 - src] == NULL)
+		{
 			return 0;
 		}
 		end = uncoveredTable[7 - src];
-		while(end->next != NULL) {
+		while (end->next != NULL)
+		{
 			prev = end;
 			end = end->next;
 		}
 
-		if(end->rank == 'A') {
-			if(end->suit == 'c') {
+		if (end->rank == 'A')
+		{
+			if (end->suit == 'c')
+			{
 				clubsFoundation = end;
 			}
-			else if(end->suit == 'd') {
+			else if (end->suit == 'd')
+			{
 				diamondsFoundation = end;
 			}
-			else if(end->suit == 'h') {
+			else if (end->suit == 'h')
+			{
 				heartsFoundation = end;
 			}
-			else if(end->suit == 's'){
+			else if (end->suit == 's')
+			{
 				spadesFoundation = end;
 			}
-			else {
+			else
+			{
 				return 0;
 			}
 		}
-		else {
-			if(compareFoundationCards(end, clubsFoundation) == 1) {
+		else
+		{
+			if (compareFoundationCards(end, clubsFoundation) == 1)
+			{
 				end->next = clubsFoundation;
 				clubsFoundation = end;
 			}
-			else if(compareFoundationCards(end, diamondsFoundation) == 1) {
+			else if (compareFoundationCards(end, diamondsFoundation) == 1)
+			{
 				end->next = diamondsFoundation;
 				diamondsFoundation = end;
 			}
-			else if(compareFoundationCards(end, heartsFoundation) == 1) {
+			else if (compareFoundationCards(end, heartsFoundation) == 1)
+			{
 				end->next = heartsFoundation;
 				heartsFoundation = end;
 			}
-			else if(compareFoundationCards(end, spadesFoundation) == 1) {
+			else if (compareFoundationCards(end, spadesFoundation) == 1)
+			{
 				end->next = spadesFoundation;
 				spadesFoundation = end;
 			}
-			else {
+			else
+			{
 				return 0;
 			}
 		}
-		if(coveredTable[7 - src] == NULL) {
+		if (prev != NULL)
+		{
+			prev->next = NULL;
+			return 1;
+		}
+		if (coveredTable[7 - src] == NULL)
+		{
 			uncoveredTable[7 - src] = NULL;
 			return 1;
 		}
-		else {
+		else
+		{
 			end = coveredTable[7 - src];
-			while(end->next != NULL) {
+			while (end->next != NULL)
+			{
 				prev = end;
 				end = end->next;
 			}
 			uncoveredTable[7 - src] = end;
-			if(prev == NULL) {
+			if (prev == NULL)
+			{
 				coveredTable[7 - src] = NULL;
 			}
-			else {
+			else
+			{
 				prev->next = NULL;
 			}
 		}
-		
-			return 1;
-		
-	} else if ((src > 0 && src < 8) && (dest > 0 && dest < 8)){
+
+		return 1;
+	}
+	else if ((src > 0 && src < 8) && (dest > 0 && dest < 8))
+	{
+		//printf("here\n");
 		end = uncoveredTable[7 - dest];
-		
-		if(end == NULL && (uncoveredTable[7 - src]->rank) == 'K') {
+
+		if (end == NULL && (uncoveredTable[7 - src]->rank) == 'K')
+		{
 			current = uncoveredTable[7 - src];
 			uncoveredTable[7 - dest] = current;
 		}
-		else {
-			while(end->next != NULL) {
+		else if (end == NULL && (uncoveredTable[7 - src]->rank) != 'K')
+		{
+			return 0;
+		}
+		else
+		{
+			while (end->next != NULL)
+			{
 				prev = end;
 				end = end->next;
 			}
-			
-			if(compareCards(uncoveredTable[7 - src], end) == 1){
+			//printf("%c%c\n", end->rank, end->suit);
+			if (compareCards(uncoveredTable[7 - src], end) == 1)
+			{
 				current = uncoveredTable[7 - src];
 				end->next = current;
-				
+				//printf("%c%c\n", current->rank, current->suit);
 			}
-			else {
+			else
+			{
 				return 0;
 			}
 		}
-			
-		if(coveredTable[7 - src] == NULL) {
+
+		if (coveredTable[7 - src] == NULL)
+		{
 			uncoveredTable[7 - src] = NULL;
 			current = uncoveredTable[7 - dest];
 		}
-		else {
+		else
+		{
 			end = coveredTable[7 - src];
-			while(end->next != NULL) {
+			prev = NULL;
+			while (end->next != NULL)
+			{
 				prev = end;
 				end = end->next;
 			}
+			//printf("%c%c\n", end->rank, end->suit);
 			uncoveredTable[7 - src] = end;
+			//printf("%p\n", prev);
+			//printf("hi\n");
+			if (prev == NULL)
+			{
 
-			if(prev == NULL) {;
 				coveredTable[7 - src] = NULL;
 			}
-			else {
+			else
+			{
+				//printf("%c%c\n", prev->rank, prev->suit);
 				prev->next = NULL;
 			}
 		}
@@ -452,39 +558,59 @@ int compareRank(char rankCard1, char rankCard2)
 
 int compareFoundationCards(node *card1, node *card2)
 {
+	if (card2 == NULL)
+	{
+		return 0;
+	}
 	char rankCard1 = (card1->rank);
+	//printf("(1)\n");
 	char rankCard2 = (card2->rank);
+	//printf("(2)\n");
 	char suitCard1 = card1->suit;
+	//printf("(3)\n");
 	char suitCard2 = card2->suit;
-
+	// printf("(4)\n");
+	// printf("%c\n", rankCard1);
+	// printf("%c\n", suitCard1);
+	// printf("%c\n", rankCard2);
+	// printf("%c\n", suitCard2);
 	if (suitCard1 == 'h' && suitCard2 == 'h')
 	{
+		//printf("(5)\n");
 		if (compareFoundationRank(rankCard1, rankCard2) == 1)
 		{
+			//printf("(6)\n");
 			return 1;
 		}
 	}
 	else if (suitCard1 == 'c' && suitCard2 == 'c')
 	{
+		//printf("(5)\n");
 		if (compareFoundationRank(rankCard1, rankCard2) == 1)
 		{
+			//printf("(6)\n");
 			return 1;
 		}
 	}
 	else if (suitCard1 == 'd' && suitCard2 == 'd')
 	{
+		//printf("(5)\n");
 		if (compareFoundationRank(rankCard1, rankCard2) == 1)
 		{
+			//printf("(6)\n");
 			return 1;
 		}
 	}
 	else if (suitCard1 == 's' && suitCard2 == 's')
 	{
+		//printf("(5)\n");
 		if (compareFoundationRank(rankCard1, rankCard2) == 1)
 		{
+			//printf("(6)\n");
 			return 1;
 		}
 	}
+	//printf("bad\n");
 	return 0;
 }
 
--- COM_S_327/project_p2/Source Files/project.c
+++ COM_S_327/project_p2/Source Files/project.c
@@ -11,6 +11,7 @@ int uncovered = 0;
 int line = 1;
 int stockC;
 int wasteC;
+int checkMovesIs2 = 0;
 
 int c;
 int turnNum = 0;
@@ -93,46 +94,270 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
+	int cardLineSize = 0;
+	int temp = 0;
+	int k = 6;
+	node *current;
+
+	for (int i = 0; i < 7; i++)
+	{
+		current = coveredTable[i];
+		while (current != NULL)
+		{
+			temp++;
+			current = current->next;
+		}
+
+		current = uncoveredTable[i];
+		while (current != NULL)
+		{
+			temp++;
+			current = current->next;
+		}
+		if (temp > cardLineSize)
+		{
+			cardLineSize = temp;
+		}
+		temp = 0;
+	}
+
 	if (found_x == 1)
 	{
 		if (found_o == 1)
 		{
+			if (checkMovesIs2 == 0)
+			{
+				fprintf(stdout, "Processed %d moves, all valid\n", numMoves);
+			}
 			fprintf(writingFp, "RULES:\n");
 			fprintf(writingFp, "  turn %d\n", turnNum);
 			fprintf(writingFp, "  unlimited\n");
 			fprintf(writingFp, "FOUNDATIONS:\n");
-			// fprintf(writingFp, "  %c%c\n", clubsFoundation->rank, clubsFoundation->suit);
-			// fprintf(writingFp, "  %c%c\n", diamondsFoundation->rank, diamondsFoundation->suit);
-			// fprintf(writingFp, "  %c%c\n", heartsFoundation->rank, heartsFoundation->suit);
-			// fprintf(writingFp, "  %c%c\n", spadesFoundation->rank, spadesFoundation->suit);
+			if (clubsFoundation == NULL)
+			{
+				fprintf(writingFp, "_c \n");
+			}
+			else
+			{
+				fprintf(writingFp, "%c%c \n", clubsFoundation->rank, clubsFoundation->suit);
+			}
+			if (diamondsFoundation == NULL)
+			{
+				fprintf(writingFp, "_d \n");
+			}
+			else
+			{
+				fprintf(writingFp, "%c%c \n", diamondsFoundation->rank, diamondsFoundation->suit);
+			}
+			if (heartsFoundation == NULL)
+			{
+				fprintf(writingFp, "_h \n");
+			}
+			else
+			{
+				fprintf(writingFp, "%c%c \n", heartsFoundation->rank, heartsFoundation->suit);
+			}
+			if (spadesFoundation == NULL)
+			{
+				fprintf(writingFp, "_s \n");
+			}
+			else
+			{
+				fprintf(writingFp, "%c%c \n", spadesFoundation->rank, spadesFoundation->suit);
+			}
+
 			fprintf(writingFp, "TABLEAU:\n");
+			for (int i = 0; i < 7; i++)
+			{
+				current = coveredTable[i];
+				while (current != NULL)
+				{
+					fprintf(writingFp, "%c", current->rank);
+					fprintf(writingFp, "%c ", current->suit);
+					current = current->next;
+				}
+				fprintf(writingFp, "| ");
+				current = uncoveredTable[i];
+				while (current != NULL)
+				{
+					fprintf(writingFp, "%c", current->rank);
+					fprintf(writingFp, "%c ", current->suit);
+					current = current->next;
+				}
+				fprintf(writingFp, "\n");
+			}
 			fprintf(writingFp, "STOCK:\n");
+			current = wasteStack;
+			while (current != NULL)
+			{
+				fprintf(writingFp, "%c", current->rank);
+				fprintf(writingFp, "%c ", current->suit);
+				current = current->next;
+			}
+			fprintf(writingFp, "| ");
+			current = stockStack;
+			while (current != NULL)
+			{
+				fprintf(writingFp, "%c", current->rank);
+				fprintf(writingFp, "%c ", current->suit);
+				current = current->next;
+			}
+			fprintf(writingFp, "\n");
 			fprintf(writingFp, "MOVES:\n");
 			fclose(writingFp);
 		}
 		else
 		{
+			if (checkMovesIs2 == 0)
+			{
+				fprintf(stdout, "Processed %d moves, all valid\n", numMoves);
+			}
 			fprintf(stdout, "RULES:\n");
 			fprintf(stdout, "  turn %d\n", turnNum);
 			fprintf(stdout, "  unlimited\n");
 			fprintf(stdout, "FOUNDATIONS:\n");
-			// fprintf(stdout, "  %c%c\n", clubsFoundation->rank, clubsFoundation->suit);
-			// fprintf(stdout, "  %c%c\n", diamondsFoundation->rank, diamondsFoundation->suit);
-			// fprintf(stdout, "  %c%c\n", heartsFoundation->rank, heartsFoundation->suit);
-			// fprintf(stdout, "  %c%c\n", spadesFoundation->rank, spadesFoundation->suit);
+			if (clubsFoundation == NULL)
+			{
+				fprintf(stdout, "_c \n");
+			}
+			else
+			{
+				fprintf(stdout, "%c%c \n", clubsFoundation->rank, clubsFoundation->suit);
+			}
+			if (diamondsFoundation == NULL)
+			{
+				fprintf(stdout, "_d \n");
+			}
+			else
+			{
+				fprintf(stdout, "%c%c \n", diamondsFoundation->rank, diamondsFoundation->suit);
+			}
+			if (heartsFoundation == NULL)
+			{
+				fprintf(stdout, "_h \n");
+			}
+			else
+			{
+				fprintf(stdout, "%c%c \n", heartsFoundation->rank, heartsFoundation->suit);
+			}
+			if (spadesFoundation == NULL)
+			{
+				fprintf(stdout, "_s \n");
+			}
+			else
+			{
+				fprintf(stdout, "%c%c \n", spadesFoundation->rank, spadesFoundation->suit);
+			}
+
 			fprintf(stdout, "TABLEAU:\n");
+			for (int i = 0; i < 7; i++)
+			{
+				current = coveredTable[i];
+				while (current != NULL)
+				{
+					fprintf(stdout, "%c", current->rank);
+					fprintf(stdout, "%c ", current->suit);
+					current = current->next;
+				}
+				fprintf(stdout, "| ");
+				current = uncoveredTable[i];
+				while (current != NULL)
+				{
+					fprintf(stdout, "%c", current->rank);
+					fprintf(stdout, "%c ", current->suit);
+					current = current->next;
+				}
+				fprintf(stdout, "\n");
+			}
 			fprintf(stdout, "STOCK:\n");
+			current = wasteStack;
+			while (current != NULL)
+			{
+				fprintf(stdout, "%c", current->rank);
+				fprintf(stdout, "%c ", current->suit);
+				current = current->next;
+			}
+			fprintf(stdout, "| ");
+			current = stockStack;
+			while (current != NULL)
+			{
+				fprintf(stdout, "%c", current->rank);
+				fprintf(stdout, "%c ", current->suit);
+				current = current->next;
+			}
+			fprintf(stdout, "\n");
 			fprintf(stdout, "MOVES:\n");
 		}
 	}
 	else if (found_o == 1)
 	{
-		fprintf(writingFp, "Processed %d moves, all valid\n", numMoves);
+		if (checkMovesIs2 == 0)
+		{
+			fprintf(stdout, "Processed %d moves, all valid\n", numMoves);
+		}
 		fprintf(writingFp, "Foundations\n");
-		// fprintf(writingFp, "%c%c %c%c %c%c %c%c\n", clubsFoundation->rank, clubsFoundation->suit,
-		// 	diamondsFoundation->rank, diamondsFoundation->suit, heartsFoundation->rank, heartsFoundation->suit,
-		// 	spadesFoundation->rank, spadesFoundation->suit);
+		if (clubsFoundation == NULL)
+		{
+			fprintf(writingFp, "_c ");
+		}
+		else
+		{
+			fprintf(writingFp, "%c%c ", clubsFoundation->rank, clubsFoundation->suit);
+		}
+		if (diamondsFoundation == NULL)
+		{
+			fprintf(writingFp, "_d ");
+		}
+		else
+		{
+			fprintf(writingFp, "%c%c ", diamondsFoundation->rank, diamondsFoundation->suit);
+		}
+		if (heartsFoundation == NULL)
+		{
+			fprintf(writingFp, "_h ");
+		}
+		else
+		{
+			fprintf(writingFp, "%c%c ", heartsFoundation->rank, heartsFoundation->suit);
+		}
+		if (spadesFoundation == NULL)
+		{
+			fprintf(writingFp, "_s ");
+		}
+		else
+		{
+			fprintf(writingFp, "%c%c ", spadesFoundation->rank, spadesFoundation->suit);
+		}
+		fprintf(writingFp, "\n");
+
 		fprintf(writingFp, "Tableau\n");
+		for (int i = 0; i < cardLineSize; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{
+				if (coveredTable[k] != NULL)
+				{
+					fprintf(writingFp, "## ");
+					coveredTable[k] = coveredTable[k]->next;
+				}
+				else
+				{
+					if (uncoveredTable[k] != NULL)
+					{
+						fprintf(writingFp, "%c%c ", uncoveredTable[k]->rank, uncoveredTable[k]->suit);
+						uncoveredTable[k] = uncoveredTable[k]->next;
+					}
+					else
+					{
+						fprintf(writingFp, ".. ");
+					}
+				}
+				k--;
+			}
+			fprintf(writingFp, "\n");
+			k = 6;
+		}
+
 		fprintf(writingFp, "Waste top\n");
 		if (wasteStack == NULL)
 		{
@@ -147,12 +372,74 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		fprintf(stdout, "Processed %d moves, all valid\n", numMoves);
+		if (checkMovesIs2 == 0)
+		{
+			fprintf(stdout, "Processed %d moves, all valid\n", numMoves);
+		}
 		fprintf(stdout, "Foundations\n");
-		fprintf(stdout, "%c%c %c%c %c%c %c%c\n", clubsFoundation->rank, clubsFoundation->suit,
-			diamondsFoundation->rank, diamondsFoundation->suit, heartsFoundation->rank, heartsFoundation->suit,
-			spadesFoundation->rank, spadesFoundation->suit);
+		if (clubsFoundation == NULL)
+		{
+			fprintf(stdout, "_c ");
+		}
+		else
+		{
+			fprintf(stdout, "%c%c ", clubsFoundation->rank, clubsFoundation->suit);
+		}
+		if (diamondsFoundation == NULL)
+		{
+			fprintf(stdout, "_d ");
+		}
+		else
+		{
+			fprintf(stdout, "%c%c ", diamondsFoundation->rank, diamondsFoundation->suit);
+		}
+		if (heartsFoundation == NULL)
+		{
+			fprintf(stdout, "_h ");
+		}
+		else
+		{
+			fprintf(stdout, "%c%c ", heartsFoundation->rank, heartsFoundation->suit);
+		}
+		if (spadesFoundation == NULL)
+		{
+			fprintf(stdout, "_s ");
+		}
+		else
+		{
+			fprintf(stdout, "%c%c ", spadesFoundation->rank, spadesFoundation->suit);
+		}
+		fprintf(stdout, "\n");
+
 		fprintf(stdout, "Tableau\n");
+
+		for (int i = 0; i < cardLineSize; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{
+				if (coveredTable[k] != NULL)
+				{
+					fprintf(stdout, "## ");
+					coveredTable[k] = coveredTable[k]->next;
+				}
+				else
+				{
+					if (uncoveredTable[k] != NULL)
+					{
+						fprintf(stdout, "%c%c ", uncoveredTable[k]->rank, uncoveredTable[k]->suit);
+						uncoveredTable[k] = uncoveredTable[k]->next;
+					}
+					else
+					{
+						fprintf(stdout, ".. ");
+					}
+				}
+				k--;
+			}
+			fprintf(stdout, "\n");
+			k = 6;
+		}
+
 		fprintf(stdout, "Waste top\n");
 		if (wasteStack == NULL)
 		{
